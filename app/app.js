b494cda797e695b1e813a143d16c935546078d93
Sean <sean@resolve.me.uk>
2015-09-16 12:11:03 +0100

    fixing popovers

--- app/app.js
+++ app/app.js
@@ -5,6 +5,7 @@ angular.module('slotDemo', [
   'ui.router',
   'ui.bootstrap',
   'ui.bootstrap.modal',
+  //'ngAnimate',
 //  'slotDemo.new',
 //  'slotDemo.phone'
 ])
@@ -53,9 +54,26 @@ angular.module('slotDemo', [
 })
     .service('opportunities', function() {
       var opportunities = this;
-      opportunities.list = [];
+      opportunities.created = [];
+      opportunities.list = function () { return opportunities.created.concat(opportunities.demoList) };
+      opportunities.getNextOpportunityId = function() { return opportunities.created.length + 1 };
+
+      opportunities.demoList = [
+        {
+          doctor: "Matt S",
+          location: "MAU",
+          procedure: "Venepuncture",
+          class: 'active',
+          status: 'Expired',
+          timelimit: '20',
+          request_time: moment().subtract(1,'hours'),
+          expiry_time:  moment().subtract(40,'minutes'),
+          accepted_by: "",
+          id: "",
+        }
+      ]
     })
-    .service('mobilePhone', function() {
+    .service('mobilePhone', function(opportunities) {
       var mobilePhone = this;
       mobilePhone.messages = [];
       mobilePhone.unreadMessages = 0;
@@ -64,8 +82,27 @@ angular.module('slotDemo', [
         mobilePhone.messages.push(messageObject);
       };
 
-      mobilePhone.addOpportunityMessage = function(procedure, location, expiry_time, doctor) {
-        mobilePhone.addMessage({direction: 'incoming', message: procedure + ' at ' + location + '.\nAttend within ' + expiry_time + '.\nAsk for ' + doctor});
+      mobilePhone.handleResponse = function(message) {
+        message = +(message);
+        console.log(angular.isNumber(message));
+        if (angular.isNumber(message) && opportunities.created[message - 1] !== undefined) {
+          if (opportunities.created[message - 1].status === 'Offered') {
+            opportunities.created[message - 1].status = 'Accepted';
+            opportunities.created[message - 1].class = 'success';
+            opportunities.created[message - 1].accepted_by = 'Med Student';
+            mobilePhone.addMessage({direction: 'incoming', message: 'Please attend ' + opportunities.created[message - 1].location + ' in ' + opportunities.created[message - 1].expiry_time.fromNow(true)
+              + ' to complete this supervised procedure. This learning opportunity has been reserved exclusively for you, please make every effort to attend.'});
+          } else {
+            mobilePhone.addMessage({direction: 'incoming', message: 'Sorry - procedure already taken this time.'});
+          }
+        } else {
+          mobilePhone.addMessage({direction: 'incoming', message: 'Sorry - this opportunity is not available.'});
+        }
+      };
+
+      mobilePhone.addOpportunityMessage = function(procedure, location, expiry_time, doctor, id) {
+        //mobilePhone.addMessage({direction: 'incoming', message: procedure + ' at ' + location + '.\nAttend within ' + expiry_time + '.\nAsk for ' + doctor});
+        mobilePhone.addMessage({direction: 'incoming', message: procedure + ' at ' + location + '.\nAttend within ' + expiry_time + '.\nAsk for ' + doctor + '\n\nTo accept reply "' + id + '"'});
         mobilePhone.unreadMessages++;
 
         console.log(mobilePhone);
@@ -100,20 +137,22 @@ angular.module('slotDemo', [
     .controller('DashboardCtrl', function(opportunities, $scope, mobilePhone) {
       $scope.opportunities = opportunities;
       $scope.mobilePhone = mobilePhone;
-      console.log(opportunities);
+      console.log(opportunities.list());
+      $scope.noOpportunities = function() { if (opportunities.list().length) { return true; } else { return false; } };
     })
     .controller('AddCtrl', function(configData, $scope, opportunities, $state, mobilePhone) {
       $scope.configData = configData;
       $scope.submit = function (opportunity) {
+        opportunity.id=opportunities.getNextOpportunityId();
         opportunity.status="Offered";
         opportunity.accepted_by="";
         opportunity.class="info";
         opportunity.request_time=moment();
         opportunity.expiry_time=moment(opportunity.request_time).add(opportunity.timelimit, 'minutes');
-        opportunities.list.push(opportunity);
+        opportunities.created.push(opportunity);
         console.log(opportunities.list);
 
-        mobilePhone.addOpportunityMessage(opportunity.procedure, opportunity.location, opportunity.expiry_time.format('HH:mm'), opportunity.doctor);
+        mobilePhone.addOpportunityMessage(opportunity.procedure, opportunity.location, opportunity.expiry_time.format('HH:mm'), opportunity.doctor, opportunity.id);
 
         $state.go('dashboard')
       }
@@ -140,6 +179,7 @@ angular.module('slotDemo', [
 
       $scope.send = function(message) {
         mobilePhone.addMessage({direction: 'outgoing', message: message});
+        mobilePhone.handleResponse(message);
         $scope.message='';
         $scope.scrollToBottom();
       };
